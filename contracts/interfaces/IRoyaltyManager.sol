270a045759c02cec5ac7dc92d1dfb6eaec0ac75f
Andy <andy.kitt@simpleweb.co.uk>
2022-05-05 09:09:42 +0100

    remove unused imports

--- contracts/interfaces/IRoyaltyManager.sol
+++ contracts/interfaces/IRoyaltyManager.sol
@@ -2,9 +2,6 @@
 
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 interface IRoyaltyManager {
     function setCustomRoyaltyPct(uint256 amount_) external;
 }
