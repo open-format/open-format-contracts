17aa6acfacaf0b5a3aad355105cc88b5a26f37ca
Andy <andy.kitt@simpleweb.co.uk>
2022-06-14 16:38:20 +0100

    install solhint

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
 require("solidity-docgen");
+require("@nomiclabs/hardhat-solhint");
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task(
