6a0a4ae629d4d55b574ecaf7aa0f19be0d3589d1
RuariM <48099261+RuariM@users.noreply.github.com>
2022-09-14 10:13:55 +0100

    Feature/create deploy and verify tasks (#32)
    * deploy and verify tasks added.
    
    * openformat -> Open Format
    
    * deploy-openformat -> deploy
    
    * adding -contract suffix to task names
    
    * fix merge conflict

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,6 +13,49 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
+
+//add deloy arguments to this object
+const token = {
+  name: "Penguins",
+  symbol: "PENG",
+  ipfsUrl:
+    "ipfs://bafyreiflupn3zziegi4fm56uhoc3yesv2jiv5ovcj23b2qq3xqcu4kq46m/metadata.json",
+  maxSupply: 100,
+  mintingPrice: "1000000000000000000"
+};
+
+task("deploy-contract", "Deploy Open Format contract").setAction(
+  async (taskArgs, hre) => {
+    const OpenFormat = await hre.ethers.getContractFactory("OpenFormat");
+
+    const openFormat = await OpenFormat.deploy(
+      token.name,
+      token.symbol,
+      token.ipfsUrl,
+      token.maxSupply,
+      token.mintingPrice
+    );
+
+    await openFormat.deployed();
+    console.log("Deployed Contract address: ", openFormat.address);
+  }
+);
+
+task("verify-contract", "Verify Open Format contract")
+  .addParam("address", "The deployed contracts address")
+  .setAction(async (taskArgs, hre) => {
+    await hre.run("verify:verify", {
+      address: taskArgs.address,
+      constructorArguments: [
+        token.name,
+        token.symbol,
+        token.ipfsUrl,
+        token.maxSupply,
+        token.mintingPrice
+      ]
+    });
+  });
+  
 const { PRIVATE_KEY, POLYGONSCAN_API_KEY } = process.env;
 
 // You need to export an object to set up your config
